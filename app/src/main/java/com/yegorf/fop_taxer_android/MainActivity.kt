46803e295019ac8309204c4610be1108d97deac6
Yehor Khohlov <y.khohlov@pnn.in.ua>
2021-01-18 19:40:27 +0200

    Initial commit

---
+++ app/src/main/java/com/yegorf/fop_taxer_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yegorf.fop_taxer_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
