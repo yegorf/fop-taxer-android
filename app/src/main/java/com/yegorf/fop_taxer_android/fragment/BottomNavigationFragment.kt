d35f5cd7c7ac05ee89235010569102f80cfd7b58
Yehor Khohlov <y.khohlov@pnn.in.ua>
2021-01-19 20:08:08 +0200

    add tabs rescaling animation

--- app/src/main/java/com/yegorf/fop_taxer_android/fragment/BottomNavigationFragment.kt
+++ app/src/main/java/com/yegorf/fop_taxer_android/fragment/BottomNavigationFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.yegorf.fop_taxer_android.FragmentNavigator
@@ -13,6 +15,8 @@ import com.yegorf.fop_taxer_android.databinding.FragmentBottomNavigationBinding
 class BottomNavigationFragment : Fragment() {
 
     lateinit var selectedTab: ImageView
+    private val animationDuration = 120L
+    private val scalingKoef = 1.2f
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +24,15 @@ class BottomNavigationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentBottomNavigationBinding.inflate(inflater)
+
+        selectedTab = binding.tabCalculation
+        animateSelectTab(selectedTab)
+
         setOnClickListeners(binding)
         return binding.root
     }
 
     private fun setOnClickListeners(binding: FragmentBottomNavigationBinding) {
-        selectedTab = binding.tabCalculation;
         if (fragmentManager != null) {
             val fragmentNavigator = FragmentNavigator(fragmentManager!!)
             binding.tabCalculation.setOnClickListener {
@@ -44,10 +51,32 @@ class BottomNavigationFragment : Fragment() {
     }
 
     private fun selectTab(newSelectedTab: ImageView) {
-        if (activity != null) {
-            selectedTab.drawable.setTint(activity!!.resources.getColor(R.color.lightGrey))
-            newSelectedTab.drawable.setTint(activity!!.resources.getColor(R.color.black))
+        if (activity != null && newSelectedTab != selectedTab) {
+            animateSelectTab(newSelectedTab)
+            animateUnselectTab(selectedTab)
             selectedTab = newSelectedTab
         }
+    }
+
+    private fun animateSelectTab(tab: ImageView) {
+        val selectAnimation = ScaleAnimation(
+            1f, scalingKoef, 1f, scalingKoef,
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        selectAnimation.duration = animationDuration
+        selectAnimation.fillAfter = true
+        tab.startAnimation(selectAnimation)
+        tab.drawable.setTint(activity!!.resources.getColor(R.color.black))
+    }
+
+    private fun animateUnselectTab(tab: ImageView) {
+        val unselectAnimation = ScaleAnimation(
+            scalingKoef, 1f, scalingKoef, 1f,
+            Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        unselectAnimation.duration = animationDuration
+        unselectAnimation.fillAfter = true
+        tab.startAnimation(unselectAnimation)
+        tab.drawable.setTint(activity!!.resources.getColor(R.color.lightGrey))
     }
 }
---
+++ app/src/main/java/com/yegorf/fop_taxer_android/tools/scale_tools.kt
@@ -0,0 +1,13 @@
+package com.yegorf.fop_taxer_android
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+
+fun convertDpToPixel(dp: Float, context: Context): Float {
+    return dp * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+}
+
+fun convertPixelsToDp(px: Float, context: Context): Float {
+    return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
+}
